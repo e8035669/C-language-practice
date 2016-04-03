af32ccec7f009ec302d8108580940c61eb32a7ab
e8035669 <e8035669@gmail.com>
2016-04-03 15:51:55 +0800

    Update p07.c

--- practice02_while/p07.c
+++ practice02_while/p07.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+	打字練習：
+	利用while loop重複處理一次輸入一個英文字母字元，並將該字元作大小寫轉換。若輸入非英文字母則停止。
+*/
 int main(int argc,char *argv[]){
 	
 	char input = 0;
 	
-	while(scanf("%c",&input)){
-		
-	}
-	
-	
-	
+	while(scanf("%c",&input){
+	if(input>='a'&&input<='z'){
+		printf("%c\n",input-32);
+	}else if(input>='A'&&input<='Z'){
+		printf("%c\n",input+32);	
+	}else break;
 	
 	return 0;
 }
