2fcb1df3734202ae70d22cc1205415d216ac71c3
e8035669 <e8035669@gmail.com>
2016-04-03 10:14:12 +0800

    add file

--- practice02_while/p05.c
+++ practice02_while/p05.c
@@ -2,8 +2,21 @@
 int main(int argc,char *argv[]){
 	
 	int A = 0,B = 0;
+	printf("因數檢查\n輸入兩個正整數A B：");
+	scanf("%d%d",&A,&B);
 	
+	int count = 0;
+	printf("%d 到 %d 之間 不是2 3 5的倍數有：",A,B);
+	if(A>B){int tmp = A;A=B;B=tmp;}
 	
+	while(A<=B){
+		if((A%2)&&(A%3)&&(A%5)){
+			printf("%d ",A);
+			count++;
+		}
+		A++; 
+	}
+	printf("\n共%d個\n",count);
 	
 	
 	return 0;
---
+++ practice02_while/p07.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[]){
+	
+	char input = 0;
+	
+	while(scanf("%c",&input))
+	
+	
+	
+	
+	return 0;
+}
